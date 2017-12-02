2c5b1c5f5b87a1f729f97f07c177a73fd5d2ebc3
Gohary <mhammedmohie@gmail.com>
2017-12-02 18:23:16 +0200

    Initial Commit

---
+++ MGHTTP/MGHTTP.h
@@ -0,0 +1,13 @@
+//
+//  MGHTTP.h
+//  MGHTTP
+//
+//  Created by Gohary on 12/2/17.
+//  Copyright © 2017 Mohammed ElGohary. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MGHTTP : NSObject
+
+@end
---
+++ MGHTTP/MGHTTP.m
@@ -0,0 +1,13 @@
+//
+//  MGHTTP.m
+//  MGHTTP
+//
+//  Created by Gohary on 12/2/17.
+//  Copyright © 2017 Mohammed ElGohary. All rights reserved.
+//
+
+#import "MGHTTP.h"
+
+@implementation MGHTTP
+
+@end
